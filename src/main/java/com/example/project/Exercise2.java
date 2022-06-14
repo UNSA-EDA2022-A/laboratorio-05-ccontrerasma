3edf62359cdd4eb61e88d25a0024336fc5441d82
ccontrerasma <88167184+ccontrerasma@users.noreply.github.com>
2022-06-14 12:32:48 -0500

    Update Exercise2.java

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -13,28 +13,32 @@ public class Exercise2 {
     }
 
     public boolean existenDuplicados(String str) {
-    	 MyStack<Character> stack = new LinkedListStack<>(); 
+    	MyStack<Character> stack = new LinkedListStack<>(); 
+    	 if(str == null) {//Si es vacio sera false, por que teoricamente no habria duplicados
+         	return false;
+         }
+    	 //	el for es para pasar char por char
          for (int i = 0; i < str.length(); i++) {
-        	
-        	 //Solo se verificara cuando haya un cierre
-             if (str.charAt(i) == ')') {
-                 char ultimo = stack.pop();
-                 int cont = 0;
-                 //Si no es abierto el contador aumentara y los borrara
-                 while (ultimo!='(') {
-                     cont++;
-                     ultimo = stack.pop();
-                 }
-                 //Cuando el contador sea 0, significa que entre dos paresentesis hay uno de rreleno
-                 if (cont < 1) {
-                     return true;
-                 }
-             }
-             //De lo contario solo los guardara en la pila
-             else {
+        	 //Cuando sea a diferente a cierra se guardara 
+        	 if(str.charAt(i) != ')') {
                  stack.push(str.charAt(i));
              }
-         }
+        	 else {//Cuando sea cierre
+        		 if (stack.top() == '(') {//Cuando encuentre '(' retornara true
+        			 return true;
+        			 }
+        		 else {//Si no es asi, eliminara hasta que que encuentre un '('
+        			  while (stack.top()!='(') {
+        				  stack.pop();
+        				  }
+        			  //Luego de eliminar a los numeros, eliminara a '(' por que este es el que le corresonde al ')'
+        			  stack.pop();	
+        			  //Despues de esto si encuentra otro '(' significa que hay duplicado y entrara al if
+        			  }
+        			
+        		 }
+        	 }
+        	 
 
          return false;
     }
